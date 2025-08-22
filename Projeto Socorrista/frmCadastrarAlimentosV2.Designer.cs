02f3e731cbeb6dc70743ea4da61baddc696aeded
anselmosz <anselmoma@outlook.com>
2025-08-22 11:51:52 -0300

    criar a branch anselmo-branch e criar a nova página de cadastro de produtos

---
+++ Projeto Socorrista/frmCadastrarAlimentosV2.Designer.cs
@@ -0,0 +1,355 @@
+﻿namespace Projeto_Socorrista
+{
+    partial class frmCadastrarAlimentosV2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gpbCamposDoProduto = new System.Windows.Forms.GroupBox();
+            this.lblNomeProduto = new System.Windows.Forms.Label();
+            this.txtNomeProduto = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblTipoProduto = new System.Windows.Forms.Label();
+            this.cbbProdutosInSys = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dtpCadastroProduto = new System.Windows.Forms.DateTimePicker();
+            this.lblDataCadastroProduto = new System.Windows.Forms.Label();
+            this.lblDataVencimentoProduto = new System.Windows.Forms.Label();
+            this.dtpVencimentoProduto = new System.Windows.Forms.DateTimePicker();
+            this.cbbUnidadesMedida = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblUnidadeReturn = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.gpbCamposDoProduto.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gpbCamposDoProduto
+            // 
+            this.gpbCamposDoProduto.Controls.Add(this.label5);
+            this.gpbCamposDoProduto.Controls.Add(this.lblUnidadeReturn);
+            this.gpbCamposDoProduto.Controls.Add(this.textBox2);
+            this.gpbCamposDoProduto.Controls.Add(this.label4);
+            this.gpbCamposDoProduto.Controls.Add(this.cbbUnidadesMedida);
+            this.gpbCamposDoProduto.Controls.Add(this.label2);
+            this.gpbCamposDoProduto.Controls.Add(this.lblDataVencimentoProduto);
+            this.gpbCamposDoProduto.Controls.Add(this.dtpVencimentoProduto);
+            this.gpbCamposDoProduto.Controls.Add(this.lblDataCadastroProduto);
+            this.gpbCamposDoProduto.Controls.Add(this.dtpCadastroProduto);
+            this.gpbCamposDoProduto.Controls.Add(this.label3);
+            this.gpbCamposDoProduto.Controls.Add(this.cbbProdutosInSys);
+            this.gpbCamposDoProduto.Controls.Add(this.textBox1);
+            this.gpbCamposDoProduto.Controls.Add(this.lblTipoProduto);
+            this.gpbCamposDoProduto.Controls.Add(this.txtNomeProduto);
+            this.gpbCamposDoProduto.Controls.Add(this.lblNomeProduto);
+            this.gpbCamposDoProduto.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbCamposDoProduto.Location = new System.Drawing.Point(3, 3);
+            this.gpbCamposDoProduto.Name = "gpbCamposDoProduto";
+            this.gpbCamposDoProduto.Size = new System.Drawing.Size(805, 266);
+            this.gpbCamposDoProduto.TabIndex = 0;
+            this.gpbCamposDoProduto.TabStop = false;
+            this.gpbCamposDoProduto.Enter += new System.EventHandler(this.gpbCamposDoProduto_Enter);
+            // 
+            // lblNomeProduto
+            // 
+            this.lblNomeProduto.AutoSize = true;
+            this.lblNomeProduto.Location = new System.Drawing.Point(419, 71);
+            this.lblNomeProduto.Name = "lblNomeProduto";
+            this.lblNomeProduto.Size = new System.Drawing.Size(109, 20);
+            this.lblNomeProduto.TabIndex = 0;
+            this.lblNomeProduto.Text = "Nome do Item";
+            // 
+            // txtNomeProduto
+            // 
+            this.txtNomeProduto.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNomeProduto.Location = new System.Drawing.Point(423, 94);
+            this.txtNomeProduto.Name = "txtNomeProduto";
+            this.txtNomeProduto.Size = new System.Drawing.Size(365, 26);
+            this.txtNomeProduto.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(227, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Cadastro de Alimentos";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(423, 155);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // lblTipoProduto
+            // 
+            this.lblTipoProduto.AutoSize = true;
+            this.lblTipoProduto.Location = new System.Drawing.Point(24, 71);
+            this.lblTipoProduto.Name = "lblTipoProduto";
+            this.lblTipoProduto.Size = new System.Drawing.Size(121, 20);
+            this.lblTipoProduto.TabIndex = 2;
+            this.lblTipoProduto.Text = "Tipo de Doação";
+            // 
+            // cbbProdutosInSys
+            // 
+            this.cbbProdutosInSys.FormattingEnabled = true;
+            this.cbbProdutosInSys.Location = new System.Drawing.Point(30, 94);
+            this.cbbProdutosInSys.Name = "cbbProdutosInSys";
+            this.cbbProdutosInSys.Size = new System.Drawing.Size(365, 28);
+            this.cbbProdutosInSys.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Dados do produto";
+            // 
+            // dtpCadastroProduto
+            // 
+            this.dtpCadastroProduto.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpCadastroProduto.Location = new System.Drawing.Point(30, 155);
+            this.dtpCadastroProduto.Name = "dtpCadastroProduto";
+            this.dtpCadastroProduto.Size = new System.Drawing.Size(128, 26);
+            this.dtpCadastroProduto.TabIndex = 6;
+            // 
+            // lblDataCadastroProduto
+            // 
+            this.lblDataCadastroProduto.AutoSize = true;
+            this.lblDataCadastroProduto.Location = new System.Drawing.Point(26, 132);
+            this.lblDataCadastroProduto.Name = "lblDataCadastroProduto";
+            this.lblDataCadastroProduto.Size = new System.Drawing.Size(139, 20);
+            this.lblDataCadastroProduto.TabIndex = 7;
+            this.lblDataCadastroProduto.Text = "Dada de Cadastro";
+            // 
+            // lblDataVencimentoProduto
+            // 
+            this.lblDataVencimentoProduto.AutoSize = true;
+            this.lblDataVencimentoProduto.Location = new System.Drawing.Point(240, 132);
+            this.lblDataVencimentoProduto.Name = "lblDataVencimentoProduto";
+            this.lblDataVencimentoProduto.Size = new System.Drawing.Size(155, 20);
+            this.lblDataVencimentoProduto.TabIndex = 9;
+            this.lblDataVencimentoProduto.Text = "Data de Vencimento";
+            // 
+            // dtpVencimentoProduto
+            // 
+            this.dtpVencimentoProduto.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpVencimentoProduto.Location = new System.Drawing.Point(237, 155);
+            this.dtpVencimentoProduto.Name = "dtpVencimentoProduto";
+            this.dtpVencimentoProduto.Size = new System.Drawing.Size(130, 26);
+            this.dtpVencimentoProduto.TabIndex = 8;
+            // 
+            // cbbUnidadesMedida
+            // 
+            this.cbbUnidadesMedida.FormattingEnabled = true;
+            this.cbbUnidadesMedida.Location = new System.Drawing.Point(30, 218);
+            this.cbbUnidadesMedida.Name = "cbbUnidadesMedida";
+            this.cbbUnidadesMedida.Size = new System.Drawing.Size(128, 28);
+            this.cbbUnidadesMedida.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(24, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Unidade de Medida";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Location = new System.Drawing.Point(237, 218);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 26);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 193);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Valor em";
+            // 
+            // lblUnidadeReturn
+            // 
+            this.lblUnidadeReturn.AutoSize = true;
+            this.lblUnidadeReturn.Location = new System.Drawing.Point(311, 193);
+            this.lblUnidadeReturn.Name = "lblUnidadeReturn";
+            this.lblUnidadeReturn.Size = new System.Drawing.Size(15, 20);
+            this.lblUnidadeReturn.TabIndex = 14;
+            this.lblUnidadeReturn.Text = "*";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 586);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(419, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 20);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Quantidade";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Cadastrar Alimentos";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(6, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(188, 38);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Relatórios";
+            this.button2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Estoque";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 315);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(188, 38);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Distribuição";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.gpbCamposDoProduto);
+            this.panel1.Location = new System.Drawing.Point(218, 77);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(811, 521);
+            this.panel1.TabIndex = 4;
+            // 
+            // frmCadastrarAlimentosV2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1041, 636);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "frmCadastrarAlimentosV2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmCadastrarAlimentosV2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.gpbCamposDoProduto.ResumeLayout(false);
+            this.gpbCamposDoProduto.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gpbCamposDoProduto;
+        private System.Windows.Forms.TextBox txtNomeProduto;
+        private System.Windows.Forms.Label lblNomeProduto;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblTipoProduto;
+        private System.Windows.Forms.ComboBox cbbProdutosInSys;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblDataCadastroProduto;
+        private System.Windows.Forms.DateTimePicker dtpCadastroProduto;
+        private System.Windows.Forms.Label lblDataVencimentoProduto;
+        private System.Windows.Forms.DateTimePicker dtpVencimentoProduto;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbbUnidadesMedida;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblUnidadeReturn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ Projeto Socorrista/frmCadastrarAlimentosV2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Socorrista
+{
+    public partial class frmCadastrarAlimentosV2 : Form
+    {
+        public frmCadastrarAlimentosV2()
+        {
+            InitializeComponent();
+        }
+
+        private void gpbCamposDoProduto_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
