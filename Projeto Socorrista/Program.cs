3f952013388a4677b73114e6e68b8d7d8a5126fc
anselmosz <anselmoma@outlook.com>
2025-09-01 11:46:18 -0300

    update Program.cs

--- Projeto Socorrista/Program.cs
+++ Projeto Socorrista/Program.cs
@@ -16,7 +16,7 @@ namespace Projeto_Socorrista
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCadastrarAlimentosV2());
+            Application.Run(new frmLoginVoluntario());
         }
     }
 }
